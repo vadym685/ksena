8bdb40fddc5b14a0375af58ba60b53a8861565cb
Vadym <vadym685@gmail.com>
2020-08-21 17:26:54 +0300

    automatic task status setting

--- modules/web/src/com/company/ksena/web/screens/task/TaskEdit.java
+++ modules/web/src/com/company/ksena/web/screens/task/TaskEdit.java
@@ -2,7 +2,9 @@ package com.company.ksena.web.screens.task;
 
 import com.company.ksena.entity.company.Company;
 import com.company.ksena.entity.task.TaskDocument;
+import com.company.ksena.entity.task.TaskStatus;
 import com.haulmont.cuba.gui.components.HasValue;
+import com.haulmont.cuba.gui.components.LookupField;
 import com.haulmont.cuba.gui.components.PickerField;
 import com.haulmont.cuba.gui.screen.*;
 import com.company.ksena.entity.task.Task;
@@ -11,12 +13,22 @@ import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
 import javax.validation.constraints.Null;
+import java.util.Objects;
 
 @UiController("ksena_Task.edit")
 @UiDescriptor("task-edit.xml")
 @EditedEntityContainer("taskDc")
 @LoadDataBeforeShow
 public class TaskEdit extends StandardEditor<Task> {
+    @Inject
+    private LookupField<TaskStatus> taskStatusField;
+
+    @Subscribe
+    public void onAfterShow(AfterShowEvent event) {
+        if (Objects.isNull(taskStatusField.getValue())) {
+            taskStatusField.setValue(TaskStatus.CREATE);
+        }
+    }
 
     private final Logger LOG = LoggerFactory.getLogger(TaskEdit.class);
 
