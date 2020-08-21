14d712f8be17a3f33032a4b40ad8a39aeb9a9769
Vadym <vadym685@gmail.com>
2020-08-21 16:02:22 +0300

    change on company, full name is non editable automatically generated from name & companyType/name

--- modules/web/src/com/company/ksena/web/screens/company/CompanyEdit.java
+++ modules/web/src/com/company/ksena/web/screens/company/CompanyEdit.java
@@ -1,11 +1,30 @@
 package com.company.ksena.web.screens.company;
 
+import com.company.ksena.entity.company.CompanyType;
+import com.haulmont.cuba.gui.components.PickerField;
+import com.haulmont.cuba.gui.components.TextField;
 import com.haulmont.cuba.gui.screen.*;
 import com.company.ksena.entity.company.Company;
+
+import javax.inject.Inject;
+import java.util.Objects;
 
 @UiController("ksena_Company.edit")
 @UiDescriptor("company-edit.xml")
 @EditedEntityContainer("companyDc")
 @LoadDataBeforeShow
 public class CompanyEdit extends StandardEditor<Company> {
+    @Inject
+    private TextField<String> nameField;
+    @Inject
+    private PickerField<CompanyType> companyTypeField;
+    @Inject
+    private TextField<String> fullNameField;
+
+    @Subscribe
+    public void onAfterShow(AfterShowEvent event) {
+       if (Objects.nonNull(nameField.getValue()) && Objects.nonNull(companyTypeField.getValue().getName())){
+           fullNameField.setValue(nameField.getValue() + " " + companyTypeField.getValue().getName());
+        }
+    }
 }
