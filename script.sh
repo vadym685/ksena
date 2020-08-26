85ba7200e447413cac4fddb08553486689e75e95
Vadym <vadym685@gmail.com>
2020-08-26 17:35:58 +0300

    change to script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Service ERP"
 #export JAVA_OPTS="-Xmx1524m -XX:MaxPermSize=256m -Xss2048k"
-export  RUN_PATH=".build/distributions/uberJar/"
+export  RUN_PATH="./build/distributions/uberJar/"
 export  RUN_APP="app.jar"
 java -jar ${RUN_PATH}${RUN_APP}  ##--server.port=5555 ${JAVA_OPTS}
 exit 0
